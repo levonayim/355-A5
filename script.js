3f0c843436b2fb58a960e2d32f0f61b96b9409ce
thomaslaurie <thomas.laurie@outlook.com>
2018-03-21 05:03:42 -0700

    update

--- script.js
+++ script.js
@@ -91,37 +91,16 @@ function calcTotals(data, names, incomeGroups) {
 	return matrix;
 }
 
-// ALL
-d3.csv(fileName, function(error, data) {
-	// d is passed by reference here
-	data.forEach(function(d) {
-		// CONVERT DATA
-		d.name = d.name;
-		d.incomeGroup = d.incomeGroup;
-		d.houseType = d.houseType;
-		d.costIncomeRatio = d.costIncomeRatio;
-
-		// NUMBERS
-		// converts to number or 0
-		d.tenureOwner = +d.tenureOwner || 0;
-		d.tenureRenter = +d.tenureRenter || 0;
-		d.tenureBand = +d.tenureBand || 0;
+function dataRatios(ratios) {
+	var ratios = [];
+	Object.keys(ratios).forEach(function(n) {
+		Object.keys(ratios[n]).forEach(function(i) {
+			ratios.push({name: n, incomeGroup: i, ratio: ratios[n][i]['ratio']});
+		});
 	});
+	return ratios;
+}
 
-	// var ratios = calcRatios(data, ['Abbotsford - Mission'], ['$10,000 to $19,999']);
-
-	// Object.keys(ratios).forEach(function(n) {
-	// 	Object.keys(ratios[n]).forEach(function(i) {
-	// 		console.log(n + ', ' + i + ', ' + ratios[n][i]['under30'] + ', ' + ratios[n][i]['total'] + ', ' + ratios[n][i]['ratio']);
-	// 	});
-	// });
-
-	var totals = calcTotals(data, ['Abbotsford - Mission'], ['$10,000 to $19,999']);
-
-	Object.keys(totals).forEach(function(h) {
-		console.log(h + ', ' + totals[h]['tenureOwner'] + ', ' + totals[h]['tenureRenter'] + ', ' + totals[h]['tenureBand']);
-	})
-});
 
 // DIMENSIONS
 var margin = {top: 30, right: 50, bottom: 50, left: 70},
@@ -153,54 +132,69 @@ var groups = svg
 	.attr('transform', 'translate(0)');
 
 
-// DATA
-//d3.csv(fileName, function(error, data) {
-d3.csv('spend30more.csv', function(error, data) {
-
-	// old
+// ALL
+d3.csv(fileName, function(error, data) {
 	data.forEach(function(d) {
-		d.income = +d.income;
-		d.fraction = +d.fraction;
+		// PREPARE DATA
+		d.name = d.name;
+		d.incomeGroup = d.incomeGroup;
+		d.houseType = d.houseType;
+		d.costIncomeRatio = d.costIncomeRatio;
+
+		// converts to number or 0
+		d.tenureOwner = +d.tenureOwner || 0;
+		d.tenureRenter = +d.tenureRenter || 0;
+		d.tenureBand = +d.tenureBand || 0;
 	});
 
-	// // new
-	// data.forEach(function(d) {
-	// 	// CONVERT DATA
-	// 	d.name = d.name;
-	// 	d.incomeGroup = d.incomeGroup;
-	// 	d.houseType = d.houseType;
-	// 	d.costIncomeRatio = d.costIncomeRatio;
+	var nameSelection = getGroups(data)['name'];
+	var incomeSelection = getGroups(data)['incomeGroup'];
 
-	// 	// NUMBERS
-	// 	d.tenureTotal = +d.tenureTotal;
-	// 	d.tenureOwner = +d.tenureOwner;
-	// 	d.tenureRenter = +d.tenureRenter;
-	// 	d.tenureBand = +d.tenureBand;
+	var ratios = calcRatios(data, nameSelection, incomeSelection);
+	var totals = calcTotals(data, nameSelection, incomeSelection);
 
-	// 	// DERIVED
+	var ratioData = dataRatios(ratios);
+
+
+	// SELECTION TO CONSOLE
+	// Object.keys(ratios).forEach(function(n) {
+	// 	Object.keys(ratios[n]).forEach(function(i) {
+	// 		console.log(n + ', ' + i + ', ' + ratios[n][i]['under30'] + ', ' + ratios[n][i]['total'] + ', ' + ratios[n][i]['ratio']);
+	// 	});
 	// });
 
-	// Scale the range of the data
-	x.domain(d3.extent(data, function(d) { return d.fraction; }));
-   //x.domain(['Under $10k', '$10k to $19,999', '$20k to $29,999', '$30k to $39,999', '$40k to $49,999', '$50k to $59,999', '$60k to $69,999', '$70k to $79,999', '$80k to $89,999', '$90k to $99,999', '$100k and over'])
-	y.domain([0, d3.max(data, function(d) { return d.tenure; })]);
+	// Object.keys(totals).forEach(function(h) {
+	// 	console.log(h + ', ' + totals[h]['tenureOwner'] + ', ' + totals[h]['tenureRenter'] + ', ' + totals[h]['tenureBand']);
+	// });
+
+	// SCALE
+	// ???
+	x.domain(d3.extent(ratioData, function(d) { return d.fraction; }));
+		var ratioArray = [];
+		Object.keys(ratios).forEach(function(n) {
+		Object.keys(ratios[n]).forEach(function(i) {
+			ratioArray.push(ratios[n][i]['ratio']);
+		});
+	});
+	y.domain([0, d3.max(ratioArray)]);
 
 
-	// PRINT IN CONSOLE ALL THE DATA
-	// sort data alphabetically
-	data.sort(function(a, b) { return d3.ascending(a.GEO_NAME, b.GEO_NAME); });
+	Object.keys(ratios).forEach(function(n) {
+		Object.keys(ratios[n]).forEach(function(i) {
+			console.log(n + ', ' + i + ', ' + ratios[n][i]['under30'] + ', ' + ratios[n][i]['total'] + ', ' + ratios[n][i]['ratio']);
+			var circles = groups
+				.selectAll('dot')
+				.append('circle')
+				.attr('r', 3)
+				.attr('cx', x(0))
+				.attr('cy', y(0))
+				.attr('id', n)
+				.style('fill', 'red');
+		});
+	});
 
-	// Add the scatterplot
-	var circles = groups
-		.selectAll('dot')
-		.data(data)
-		.enter()
-		.append('circle')
-		.attr('r', 3) // how big the circles will be
-		.attr('cx', function(d) { return x(+d.fraction); })
-		.attr('cy', function(d) { return y(+d.tenure); })
-		.attr('id', function(d) { return d.GEO_NAME;})
-		.style('fill', function(d) { return 'red'; });
+
+
 	
 //INTERACTIONS
 	// mouse over functionality
@@ -284,5 +278,4 @@ d3.csv('spend30more.csv', function(error, data) {
 			.style('font-weight', 'bold')
 			.text('Tenure');
 			//.text('Fraction of the total population spending > 30% on housing');
-
 });
